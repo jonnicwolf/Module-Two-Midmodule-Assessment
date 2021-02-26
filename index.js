ea3e831b0ee1a681404b7c9aa11e23967a04ffe9
Jon Narine <jonnicwolf@gmail.com>
2021-02-26 13:44:38 -0500

    working on refreshing dice

--- index.js
+++ index.js
@@ -1,76 +1,67 @@
-// const inputArr = []
-// const numberSnek = () => {
-//     const input = Number(document.querySelector('#number-statistics-number-input').value);
-//     const ul = document.querySelector('#number-statistics-numbers')
-//     let avg = document.querySelector('#number-statistics-average')
-//     let max = document.querySelector('#number-statistics-maximum')
-//     let min = document.querySelector('#number-statistics-minimum')
 
-//     inputArr.unshift(input)
-//     let li = document.createElement('li')    
-//     li.textContent = input
-//     ul.appendChild(li)
-
-//     let sorter = inputArr.sort((lo, hi) => { return hi - lo })     
-
-//     max.textContent = `Maximum: ${sorter[0]}`
-//     min.textContent = `Minimum: ${sorter[sorter.length - 1]}`    
-//     avg.textContent = `Average: ${sorter.reduce((a, b) => { return (a + b) })/sorter.length}`    
-// }
-//document.addEventListener('DOMContentLoaded', () => {    
-    let arr = []
-    const diceRoll = () => {
+document.addEventListener('DOMContentLoaded', () => {
+    const d6 = () => {
+        let roll = Math.floor(Math.random() * 6) + 1
+        return roll
+    }
+    //let arr = []
+   `` const diceRoll = () => {
         const input = Number(document.querySelector('#input-num').value)
-        const output = document.querySelector('#sum-para') 
+        const output = document.querySelector('#sum-para')
         const outputDice = document.querySelector('#dice-para')
         const outputHistory = document.querySelector('#output-sum-history')
-        
-        const d6 = () => {
-            let roll = Math.floor(Math.random() * 6) + 1
-            return roll
-        }
+
+        //make the dice
         // const d1 = String.fromCodePoint(0x2680)
         // const d2 = String.fromCodePoint(0x2681)
         // const d3 = String.fromCodePoint(0x2682)
         // const d4 = String.fromCodePoint(0x2683)
         // const d5 = String.fromCodePoint(0x2684)
+    
         // const d6 = String
-        //let arr = []    
+        let arr = []
+        console.log(`${arr} arr before loop`)
+        const inputReset = document.getElementById('dice-para')
+        inputReset.value = ''
         for (let i = 0; i < input; i++) {
             let roll = d6()
-            switch (roll) {                
+            switch (roll) {
                 case 1:
-                    outputDice.textContent = String.fromCodePoint(0x2680)
+                    
+                    outputDice.textContent += String.fromCodePoint(0x2680)
+                    console.log(`d1 here`)
                     break;
                 case 2:
                     outputDice.textContent += String.fromCodePoint(0x2681)
+                    console.log(`d2 here`)
                     break;
                 case 3:
                     outputDice.textContent += String.fromCodePoint(0x2682)
+                    console.log(`d3 here`)
                     break;
                 case 4:
                     outputDice.textContent += String.fromCodePoint(0x2683)
+                    console.log(`d4 here`)
                     break;
                 case 5:
                     outputDice.textContent += String.fromCodePoint(0x2684)
+                    console.log(`d5 here`)
                     break;
                 case 6:
                     outputDice.textContent += String.fromCodePoint(0x2685)
+                    console.log(`d6 here`)
                     break;
             }
             arr.push(roll)
-            let nodeTest = Node.isNode(outputHistory)
-            if (nodeTest) {
-                
-          //       
-          //}
+            console.log(`${arr} arr after loop`)
         }
-        //let log = []
+        
         const sum = arr.reduce((a, b) => a + b)
-        //log.unshift()
+        console.log(`${sum} sum here `)
+        
         output.textContent = `Sum = ${sum}`
-    }
-//})
+   }
+})
 
 
 
